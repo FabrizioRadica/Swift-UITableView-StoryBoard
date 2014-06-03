ad75ca552249c81a720cd0aecac0e14c40c3f39c
FabrizioRadica <fabrizio@radicadesign.com>
2014-06-03 10:51:32 +0200

    Initial Commit

---
+++ PrimoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimoSwift
+//
+//  Created by fabrizio on 03/06/14.
+//  Copyright (c) 2014 Fabrizio Radica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
