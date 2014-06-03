a9d4b9a0028ffd2c4ca372abea3c079b7183d1dd
FabrizioRadica <fabrizio@radicadesign.com>
2014-06-04 01:08:13 +0200

    Add textScroll.editable=false

--- PrimoSwift/myViewController.swift
+++ PrimoSwift/myViewController.swift
@@ -42,6 +42,7 @@ class myViewController: UIViewController {
         textScroll.text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
         textScroll.backgroundColor=UIColor(white: 0.0, alpha: 0.5)
         textScroll.textColor=UIColor(white: 1.0, alpha: 1.0)
+        textScroll.editable=false
         self.view.addSubview(textScroll)
 
     }
