f10d0cacf3f467f0181fc69cd5e5e9c6070f5ef8
FabrizioRadica <fabrizio@radicadesign.com>
2014-06-03 11:36:24 +0200

    Swift+UiTableView+StoryBoard
    First test with swift.
    UITableView and StoryBoard

--- PrimoSwift/ViewController.swift
+++ PrimoSwift/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView : UITableView
+    
+    var items: String[] = ["Primo", "Secondo", "terzo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        cell.textLabel.text = self.items[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        
+        performSegueWithIdentifier("miaview", sender: self.view)
+        println("Hai selezionato: \(indexPath.row)!")
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
